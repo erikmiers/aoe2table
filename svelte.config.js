0f3bcee59ed9211f5b00ac2f31a1972a8f225f36
Erik Miers <erik.miers@gempa.de>
2026-09-10 11:37:37 +0200

    [config] Change some config values

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,33 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+export default defineConfig({
+  plugins: [svelte()],
+  base: '/aoe2table/',   // ← important: include the trailing slash
+})
+
+
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import adapter from '@sveltejs/vite-plugin-svelte';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   kit: {
+//     adapter: adapter(),
+//     paths: {
+//       base: process.env.BASE_PATH || ''   // or hardcode '/repo-name'
+//     }
+//   },
+//   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+//   // for more information about preprocessors
+//   preprocess: vitePreprocess(),
+// };
+
+// export default config;
+
+// // import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// // export default {
+// //   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+// //   // for more information about preprocessors
+// //   preprocess: vitePreprocess(),
+// // }
